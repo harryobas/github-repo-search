af72279803b67ec1a6ddb31cb5114967da7d509e
harry obaseki <harryobas@gmail.com>
2023-10-02 19:59:57 +0100

    minor refactoring

--- app.rb
+++ app.rb
@@ -8,22 +8,28 @@ configure do
     set :server, :puma
 end
 
+helpers do 
+    def data_from_github(search_term)
+
+        github_token = ENV['GITHUB_TOKEN']
+        github_api_url = "#{ENV['GITHUB_API_URL']}#{search_term}"
+
+        HTTParty.get(
+            github_api_url,
+            headers: {
+                'Authorization': "token #{github_token}"
+            }
+        )
+    end
+end
+
 get '/' do
     erb :new_search
 end
 
 post '/search' do
     search_term = params[:search_term]
-    github_token = ENV['GITHUB_TOKEN']
-    github_api_url = "#{ENV['GITHUB_API_URL']}#{search_term}"
-
-    response = HTTParty.get(
-        github_api_url,
-        headers: {
-            'Authorization': "token #{github_token}"
-        }
-    )
-
+    response = data_from_github(search_term)
     @repos = response.parsed_response['items']
 
     erb :results

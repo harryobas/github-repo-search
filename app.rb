7aed60c2f1efbc898970bfbafe183407fcb0c516
harry obaseki <harryobas@gmail.com>
2023-10-02 11:33:44 +0100

    implemented search results endpoint

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
 require 'httparty'
 require 'json'
+require 'dotenv/load'
+
+require sinatra/reloader if development?
 
 get '/' do
     erb :new_search
+end
+
+post '/search' do
+    search_term = params[:search_term]
+    github_token = ENV['GITHUB_TOKEN']
+    github_api_url = "#{ENV['GITHUB_API_URL']}#{search_term}"
+
+    response = HTTParty.get(
+        github_api_url,
+        
+    )
+
+    @repos = response.parsed_response['items']
+
+    erb :results
 end
 
